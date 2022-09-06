dba028abc4cff5b024ef5c7385e7b97736c5ebf5
zliu1022 <zliu1022@gmail.com>
2022-09-06 11:00:22 +0800

    Update t_query.py

--- t_query.py
+++ t_query.py
@@ -3,6 +3,9 @@
 
 from pymongo import MongoClient
 from pprint import pprint
+from datetime import datetime, timedelta
+import chinese_calendar as calendar
+
 
 if __name__ == '__main__':
     ts_code = '603060.SH'
@@ -65,8 +68,41 @@ if __name__ == '__main__':
         for x in ref['day']:
             if x['date'] <= end_date and x['date'] >= start_date:
                 print(x['date'], x['open'], x['close'])
-
     print()
+
+    ts_code = '002475.SZ'
+    start_date = '20180101'
+    end_date = '20220901'
+    v = {'ts_code': ts_code}
+    ref = db.day.find_one(v)
+    yesterday_date = datetime.strptime(start_date, '%Y%m%d')
+    if ref == None:
+        print('None')
+    else:
+        print('db.day.find_one', ref['ts_code'], len(ref['day']))
+        for x in ref['day'][::-1]:
+            if x['date'] <= end_date and x['date'] >= start_date:
+                print(x['date'])
+                today_date = datetime.strptime(x['date'], '%Y%m%d')
+                delta_days = (today_date - yesterday_date).days
+                if delta_days == 0:
+                    print(x['date'], ' = ', start_date)
+                    yesterday_date = today_date
+                    continue
+                if delta_days == 1:
+                    yesterday_date = today_date
+                    continue
+                if delta_days != 1:
+                    for i in range(1, delta_days):
+                        tmp_date = yesterday_date + timedelta(days=i)
+                        on_holiday, holiday_name = calendar.get_holiday_detail(tmp_date)
+                        tmp_weekday_str = tmp_date.strftime('%A')
+                        if tmp_weekday_str != 'Saturday' and tmp_weekday_str != 'Sunday' and on_holiday == False:
+                            print('Error', ts_code, datetime.strftime(tmp_date, '%Y%m%d'), tmp_date.strftime('%A'))
+                        print(delta_days-1, datetime.strftime(tmp_date, '%Y%m%d'), tmp_date.strftime('%A'), 'holiday', on_holiday, holiday_name)
+                    yesterday_date = today_date
+    print()
+    quit()
 
     unwind_stage = { '$unwind': '$day' }
     match_stage = {   
