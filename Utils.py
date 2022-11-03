b2120dd4dd55d7c32e48359f69219c651000b866
zliu1022 <zliu1022@gmail.com>
2022-11-03 20:33:38 +0800

    Update Utils.py

--- Utils.py
+++ Utils.py
@@ -4,11 +4,11 @@
 import pandas as pd
 import numpy  as np
 
-def create_buy_table(interval=0.025, inc_perc=1.1, dec_buy_ratio=5):
+def create_buy_table(interval=0.025, inc_perc=1.1, dec_buy_ratio=5, base_price=100.0):
     num = round(1/interval) + 1
 
     a = pd.DataFrame({'dec_perc':np.arange(num)*interval}) # 下跌幅度，似乎和 stat_chg 的dec有理解差异
-    a.insert(1, 'cur_price',   (1-a.dec_perc)*100) # 根据下跌计算的当前价格
+    a.insert(1, 'cur_price',   (1-a.dec_perc) * base_price) # 根据下跌计算的当前价格
     a.insert(2, 'buy_qty',     np.round(np.exp2(np.arange(num)/dec_buy_ratio))*100) # 买入数量
     a.insert(3, 'acum_qty',    a.buy_qty.cumsum()) # hold qty, 累计的、持有数量
     a.insert(4, 'sell_price',  a.cur_price*inc_perc) # 卖出价格
