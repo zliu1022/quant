be5063150dbcc59f6edb118f4d69a9c21d3b5d21
zliu1022 <zliu1022@gmail.com>
2023-06-12 17:35:40 +0800

    Update t_StockRecover.py

--- t_StockRecover.py
+++ t_StockRecover.py
@@ -8,13 +8,15 @@ if __name__ == '__main__':
     sq = StockQuery()
 
     # 检查复权数据，amount不复权
-    ts_code    = '002475.SZ'
-    start_date = '20220711'
-    end_date   = '20220714' # bonus 中的日期是 20220713
+    #ts_code    = '002475.SZ'
+    #start_date = '20220711'
+    #end_date   = '20220714' # bonus 中的日期是 20220713
+
+    ts_code    = '601919.SH'
+    start_date = '20200101'
+    end_date   = '20230602'
 
     df_day   = sq.query_day_code_date_df(ts_code, start_date, end_date)
-    print(df_day)
     df_bonus = sq.query_bonus_code_df(ts_code)
     df_forw  = recover_price_forward(df_day, df_bonus)
-    print(df_forw)
 
