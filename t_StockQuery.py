1cd2c51b077b7d37bf00f7ec43c69f81b8d2d7ec
zliu1022 <zliu1022@gmail.com>
2023-07-07 15:23:37 +0800

    Update t_StockQuery.py

--- t_StockQuery.py
+++ t_StockQuery.py
@@ -2,47 +2,19 @@
 # -*- coding: utf-8 -*-
 
 from StockQuery import StockQuery
-import numpy as np
+from pprint import pprint
 
 if __name__ == '__main__':
     sq = StockQuery()
 
-    # query dayline
-    ts_code    = '601919.SH'
-    start_date = '20200101'
-    end_date   = '20200101'
-    #ref = sq.query_day_code_date(ts_code, start_date, end_date)
+    start_date = '20230101'
+    end_date   = '20230201'
+    ret = sq.query_bonus_date(start_date, end_date)
+    pprint(ret[0])
 
-    # select mktvalue [0, 10) 亿
-    v1 = 0; v2 = 10
-    ref = sq.select_mktvalue(v1, v2)
-    print('[{:4d},   {:4d}) {:4d}'.format(v1, v2, len(ref)))
-
-    v1 = 10; v2 = 20
-    ref = sq.select_mktvalue(v1, v2)
-    print('[{:4d},   {:4d}) {:4d}'.format(v1, v2, len(ref)))
-
-    v1 = 20; v2 = 50
-    ref = sq.select_mktvalue(v1, v2)
-    print('[{:4d},   {:4d}) {:4d}'.format(v1, v2, len(ref)))
-
-    v1 = 50; v2 = 100
-    ref = sq.select_mktvalue(v1, v2)
-    print('[{:4d},   {:4d}) {:4d}'.format(v1, v2, len(ref)))
-
-    v1 = 100; v2 = 200
-    ref = sq.select_mktvalue(v1, v2)
-    print('[{:4d},   {:4d}) {:4d}'.format(v1, v2, len(ref)))
-
-    v1 = 200; v2 = 500
-    ref = sq.select_mktvalue(v1, v2)
-    print('[{:4d},   {:4d}) {:4d}'.format(v1, v2, len(ref)))
-
-    v1 = 500; v2 = 1000
-    ref = sq.select_mktvalue(v1, v2)
-    print('[{:4d},   {:4d}) {:4d}'.format(v1, v2, len(ref)))
-
-    v1 = 1000; v2 = np.inf
-    ref = sq.select_mktvalue(v1, v2)
-    print('[{:4d},    inf) {:4d}'.format(v1, len(ref)))
+    ts_code    = '831445.BJ'
+    start_date = '20210101'
+    end_date   = '20230201'
+    ret = sq.query_bonus_code_date(ts_code, start_date, end_date)
+    pprint(ret[0])
 
