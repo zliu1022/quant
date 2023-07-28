b7d4036a9923e0e1d991ca039cd787ad21224dfd
zliu1022 <zliu1022@gmail.com>
2023-07-28 15:49:49 +0800

    Create t_StockQuery_bd.py

---
+++ t_StockQuery_bd.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from StockQuery import StockQuery
+from pprint import pprint
+
+def drop_bd_useless(df_cur):
+    df_new = df_cur.drop(columns=['_id', '序号', '现价', '涨跌幅', '涨跌', '涨速', '换手', '量比', '振幅', '成交额', '流通股', '流通市值'])
+    return df_new
+
+if __name__ == '__main__':
+    sq = StockQuery()
+
+    ts_code = "000060.SZ"
+    df_ref = sq.query_bd_tscode(ts_code.split('.')[0])
+    #df_ref = drop_bd_useless(df_ref)
+    print(ts_code, df_ref.iloc[0]['名称'], 'pe', df_ref.iloc[0]['市盈率'])
+    for index, row in df_ref.iterrows():
+        bd_code = row['板块代码']
+        df = sq.query_bdlist(bd_code)
+        print("    ", bd_code, df.iloc[0]['概念名称'])
+    print()
+
+    bd_code = "301582"
+    #bd_code = "300248"
+    bd_code_list = [
+        "300809", #小金属概念
+        "301511", # 金属镍
+        "301577", #金属铜
+        "301582", #金属锌
+        "302174", #钴
+        "308864"  #金属
+    ]
+    for bd_code in bd_code_list:
+        df = sq.query_bdlist(bd_code)
+        print(bd_code, df.iloc[0]['概念名称'])
+
+        df_ref = sq.query_bd_bdcode(bd_code)
+        df_ref = drop_bd_useless(df_ref)
+        pprint(df_ref)
+        print()
