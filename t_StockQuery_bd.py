7fa0fa69358b031e8f2b7ca72cda8c43554d2b9b
zliu1022 <zliu1022@gmail.com>
2023-08-01 16:32:05 +0800

    Update t_StockQuery_bd.py

--- t_StockQuery_bd.py
+++ t_StockQuery_bd.py
@@ -8,10 +8,7 @@ def drop_bd_useless(df_cur):
     df_new = df_cur.drop(columns=['_id', '序号', '现价', '涨跌幅', '涨跌', '涨速', '换手', '量比', '振幅', '成交额', '流通股', '流通市值'])
     return df_new
 
-if __name__ == '__main__':
-    sq = StockQuery()
-
-    ts_code = "000060.SZ"
+def tscode(sq, ts_code):
     df_ref = sq.query_bd_tscode(ts_code.split('.')[0])
     #df_ref = drop_bd_useless(df_ref)
     print(ts_code, df_ref.iloc[0]['名称'], 'pe', df_ref.iloc[0]['市盈率'])
@@ -21,8 +18,21 @@ if __name__ == '__main__':
         print("    ", bd_code, df.iloc[0]['概念名称'])
     print()
 
+if __name__ == '__main__':
+    sq = StockQuery()
+
+    ts_code = "000060.SZ"
+    tscode(sq, ts_code)
+
     bd_code = "301582"
     #bd_code = "300248"
+    df = sq.query_bdlist(bd_code)
+    print(bd_code, df.iloc[0]['概念名称'])
+    df_ref = sq.query_bd_bdcode(bd_code)
+    df_ref = drop_bd_useless(df_ref)
+    pprint(df_ref)
+    quit()
+
     bd_code_list = [
         "300809", #小金属概念
         "301511", # 金属镍
