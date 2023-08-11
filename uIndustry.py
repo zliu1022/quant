8cc38d980e983c07be7be6953bca4b0dcb0d2a54
zliu1022 <zliu1022@gmail.com>
2023-08-11 18:39:00 +0800

    Update uIndustry.py

--- uIndustry.py
+++ uIndustry.py
@@ -9,18 +9,26 @@ from uCode import tscode
 if __name__ == '__main__':
     sq = StockQuery()
 
+    industry = '铅锌'
+    mv_min = 0
+    mv_max = 99999
     if len(sys.argv) == 2:
         industry = sys.argv[1]
+    elif len(sys.argv) == 4:
+        industry = sys.argv[1]
+        mv_min = float(sys.argv[2])
+        mv_max = float(sys.argv[3])
     else:
         print('./uXX industry')
         print('industry -> code_list')
-        industry = '铅锌'
 
     df = sq.query_industry_df(industry)
-    print(df)
     df_mv = sq.query_mktvalue_industry(industry)
+    '''
+    print(df)
     print(df_mv)
     quit()
+    '''
 
     for i, row in df.iterrows():
         code = row['ts_code']
@@ -30,6 +38,6 @@ if __name__ == '__main__':
         for i, row in df_mv.iterrows():
             arr = row['mv']
             for i in arr:
-                if i['ts_code'] == code:
+                if i['ts_code'] == code and i['mktvalue'] < mv_max and i['mktvalue'] > mv_min:
                     print('    ', row['start_date'], row['end_date'], i['mktvalue'])
 
