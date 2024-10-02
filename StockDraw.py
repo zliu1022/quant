4f273ce0d97e554a47a7c1a3a2519a725b533746
zliu1022 <zliu1022@gmail.com>
2024-10-02 10:48:58 +0800

    优化draw_price_amount_withchg

--- StockDraw.py
+++ StockDraw.py
@@ -10,7 +10,7 @@ import sys
 from Utils import RecTime
 from StockRecover import recover_price_forward
 from StockRecover import recover_price_backward
-from StockSim import stat_chg
+from StockSim import stat_chg, sim_chg, sim_chg_monthly
 
 class StockDraw:
     def __init__(self):
@@ -94,28 +94,41 @@ def draw_price_amount_withchg(ts_code, df_day, df_chg, chg_perc):
     ax2 = fig.add_axes([0.1, 0.05, 0.85, 0.2])
 
     # https://www.w3schools.com/colors/colors_names.asp
-    ax1.plot(df_day.index, df_day.high, linewidth=1, alpha=0.5, color='orangered', label='high')
-    ax1.plot(df_day.index, df_day.low,  linewidth=1, color='lightgreen',  label='low')
+    ax1.plot(df_day.date, df_day.high, linewidth=1, alpha=0.5, color='orangered', label='high')
+    ax1.plot(df_day.date, df_day.low,  linewidth=1, color='lightgreen',  label='low')
 
     first_x = datetime.strptime(df_chg.index[0], '%Y%m%d')
     for i in range(len(df_chg.index)):
         arr_x, arr_y = [], []
         arr_incx, arr_incy = [], []
 
+        # 绘制第一次买入->最低价
         arr_x.append(df_chg.index[i])
-        arr_x.append(df_chg.min_date.iloc[i])
         arr_y.append(df_chg.firstmin.iloc[i])
+        arr_x.append(df_chg.min_date.iloc[i])
         arr_y.append(df_chg['min'].iloc[i])
         ax1.plot(arr_x, arr_y, linestyle='dotted', linewidth=1, color='royalblue')
 
+        # 绘制第一次买入日期和价格
         cur_x = datetime.strptime(df_chg.index[i], '%Y%m%d')
-        ax1.text((cur_x-first_x).days*0.65, arr_y[0]*1+0.5, arr_x[0], ha='left', rotation=20, fontsize=7, color='gray')
+        ax1.text((cur_x-first_x).days*0.65+10, arr_y[0]*1+0.5, arr_x[0]+" {:.3f}".format(arr_y[0]), ha='left', rotation=20, fontsize=7, color='gray')
+
+        # 绘制最低价买入日期和价格
+        min_x = datetime.strptime(df_chg.min_date.iloc[i], '%Y%m%d')
+        ax1.text((min_x-first_x).days*0.65, arr_y[1]*1-0.5, arr_x[1]+" {:.3f}".format(arr_y[1]), ha='left', rotation=20, fontsize=7, color='gray')
 
         arr_incx.append(df_chg.min_date.iloc[i])
         arr_incy.append(df_chg['min'].iloc[i])
         arr_incx.append(df_chg.max_date.iloc[i])
         arr_incy.append(df_chg['max'].iloc[i])
-        ax1.plot(arr_incx, arr_incy, linestyle='dotted', linewidth=1.5, color='red')
+
+        # 最后一次买入未卖出，设置时间为20500101
+        if arr_incx[1] != '20500101':
+            # 绘制最低价->最高价
+            ax1.plot(arr_incx, arr_incy, linestyle='dotted', linewidth=1.5, color='red')
+            max_x = datetime.strptime(df_chg.max_date.iloc[i], '%Y%m%d')
+            # 绘制最高价卖出日期和价格
+            ax1.text((max_x-first_x).days*0.65, arr_incy[1]*1+0.5, arr_incx[1]+" {:.3f}".format(arr_incy[1]), ha='left', rotation=20, fontsize=7, color='gray')
 
         ax1.plot(df_chg.min_date.iloc[i], df_chg['min'].iloc[i], 'o', c='r', markersize=1.1)
 
@@ -123,7 +136,7 @@ def draw_price_amount_withchg(ts_code, df_day, df_chg, chg_perc):
         title=ts_code)
     leg = ax1.legend(loc='upper right', frameon=False)
 
-    ax2.bar(df_day.index, df_day.amount, alpha=0.5, width=0.6, label='amount')
+    ax2.bar(df_day.date, df_day.amount, alpha=0.5, width=0.6, label='amount')
     ax2.set(xlabel='date', ylabel='amount')
     leg2 = ax2.legend(loc='upper right', frameon=False)
 
@@ -131,12 +144,12 @@ def draw_price_amount_withchg(ts_code, df_day, df_chg, chg_perc):
     day_len = len(df_day.index)
 
     index_date = datetime.strptime(df_day.date[0], '%Y%m%d')
-    arr_ticks.append(df_day.index[0])
+    arr_ticks.append(df_day.date[0])
 
     for i in range(day_len):
         cur_date = datetime.strptime(df_day.date[i], '%Y%m%d')
         if (cur_date-index_date).days > 87 and cur_date.day<=3:
-            arr_ticks.append(df_day.index[i])
+            arr_ticks.append(df_day.date[i])
             index_date = cur_date
 
     ax1.axes.xaxis.set_ticks(arr_ticks)
@@ -190,8 +203,11 @@ def draw_example(ts_code, start_date, end_date, chg_perc):
 
     # 统计上涨 0.1 幅度下，次数、最大下跌幅度、最大下跌天数
     df_chg, total_num, max_dec_perc, max_dec_days = stat_chg(df_forw, start_date, chg_perc)
-    print('{} {} -{:.1f}% {:3d}'.format(ts_code, total_num, max_dec_perc, max_dec_days))
+    print('{} total_num {} max_dec_perc -{:.1f}% max_dec_days {:3d}'.format(ts_code, total_num, max_dec_perc, max_dec_days))
+    print('df_chg')
     print(df_chg)
+    print()
+
     draw_price_amount_withchg(ts_code, df_forw, df_chg, chg_perc)
 
     # 后复权
@@ -213,15 +229,16 @@ def draw_example(ts_code, start_date, end_date, chg_perc):
     rt.show_s()
 
 if __name__ == '__main__':
+    sq = StockQuery()
     # 20240802: ./StockDraw.py 002475.SZ 1.55 0.03 运行成功，并核对了前复权数据
     if len(sys.argv) == 4:
         start_date = '20201013'
-        end_date   = '20240802'
+        end_date   = '20241001'
         ts_code  = sys.argv[1]        # 002475.SZ
         chg_perc = float(sys.argv[2]) # 1.55
         interval = float(sys.argv[3]) # 0.03
         draw_example(ts_code, start_date, end_date, chg_perc)
-        #sim_chg_monthly(ts_code, start_date, end_date, chg_perc, interval)
+        #sim_chg_monthly(sq, ts_code, start_date, end_date, chg_perc, interval)
         quit()
   
     ts_code    = None       # all stocks
@@ -229,7 +246,6 @@ if __name__ == '__main__':
     end_date   = '20230602'
     chg_perc   = 0.55
     interval   = 0.03
-    sq = StockQuery()
 
     if ts_code == None:
         title_str = 'stat-{:.1f}%-{}-mv1000'.format(chg_perc*100, interval)
@@ -247,7 +263,7 @@ if __name__ == '__main__':
     else:
         code_list = [{'ts_code':ts_code}]
 
-    df_stat = sim_chg(code_list, start_date, end_date, chg_perc, interval)
+    df_stat = sim_chg(sq, code_list, start_date, end_date, chg_perc, interval)
     draw_stat_chg(df_stat, title_str)
     #df_stat.to_csv(title_str + '.csv', index=False)
 
